09de458618f40fb98658788342a4979f2711bea1
KevinX8 <pgnmaster24@gmail.com>
2022-03-12 19:40:07 +0000

    finally fix logo

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import Image from '../../elements/Image';
-import LogoImg from "./../../../assets/images/logo.svg";
+import LogoImg from "./../../../assets/images/logo.png";
 
 const Logo = ({
   className,
